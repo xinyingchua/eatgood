da58ac61fa4d50ca22ede3845922e35af485c7b1
xinyingchua <79414040+xinyingchua@users.noreply.github.com>
2021-06-15 11:20:09 +0800

    added virtual in recipes model

--- models/recipes.js
+++ models/recipes.js
@@ -7,6 +7,19 @@ const recipeSchema = new mongoose.Schema({
     category: { type: String, required: true },
     image: { type: String, required: true },
   })
+
+
+// to have an option to load user model when we use product rating model
+// use virtual if you have collection that is dependent on another
+recipeSchema.virtual('user', {
+  ref: 'User',
+  localField: 'user_id', // property that we use to search for users
+  foreignField: '_id',
+  justOne: true // mongo to return one result 
+
+})
+
+recipeSchema.set('toObject', {virtuals:true}) // whenever recipe model is called, virtual will show
   
   const RecipeModel = mongoose.model('Recipe', recipeSchema)
   
